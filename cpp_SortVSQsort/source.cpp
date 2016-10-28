1cafc2944064395d0503ffa03358d009466cc71d
junjw1 <junjw1@daum.net>
2016-10-28 16:56:19 +0900

    avoid OLD style

--- cpp_SortVSQsort/source.cpp
+++ cpp_SortVSQsort/source.cpp
@@ -5,9 +5,14 @@
 
 using namespace std;
 
-int compare (const void * a, const void * b)
-{
-  return ( *(int*)a - *(int*)b );
+int compare (const void * a, const void * b){
+  return ( *(int*)a - *(int*)b ); //오름차순
+//   return ( *(int*)b - *(int*)a ); //내림차순
+}
+
+bool myfunc(int i, int j){
+    return i<j; //오름차순
+    // return i>j; //내림차순
 }
 
 int main(){
@@ -15,6 +20,7 @@ int main(){
     int T;
     int N;    
     vector<int> a;    
+    vector<int>::iterator iter;
 
     freopen("input.txt","r",stdin);
 
@@ -30,14 +36,17 @@ int main(){
             scanf("%d", &value);
             a.push_back(value);
         }
-        for(int i=0; i<a.size(); i++) printf("%d ", a[i]);
+
+        //이게 바로 STL 스타일~~
+        for(iter = a.begin(); iter != a.end(); iter++) printf("%d ", *iter);
         printf("\n");
 
         // 첫번째 인자에는 iterator 대신 실제 포인터를! 
         // qsort(&a[0], a.size(), sizeof(a[0]), compare); //cstdlib
 
-        sort(a.begin(), a.end()); //algorithm
+        sort(a.begin(), a.end(), myfunc); //algorithm, myfunc 안넣으면 디폴트 오름차순
 
+        //올드하다~~
         for(int i=0; i<a.size(); i++) printf("%d ", a[i]);
         printf("\n");
 
